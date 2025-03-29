94a212f31550c45d6abb04e350cde9debf2414f2
mi-wada <mtak.wd@gmail.com>
2025-03-29 13:11:35 +0900

    update

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -2,32 +2,10 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
-
-	scanner.Scan()
-	n, _ := strconv.Atoi(scanner.Text())
-	m, _ := strconv.Atoi(scanner.Text())
-
-	stack := make([]int, 100)
-	for i := 0; i < q; i++ {
-		scanner.Scan()
-		t, _ := strconv.Atoi(scanner.Text())
-		switch t {
-		case 1:
-			scanner.Scan()
-			x, _ := strconv.Atoi(scanner.Text())
-			stack = append(stack, x)
-		case 2:
-			last := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-			fmt.Println(last)
-		}
-	}
 }
